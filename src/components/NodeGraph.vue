d10cecbe7c22846030b538a9a6c640f5d85451cb
awagen <awagen@posteo.net>
2021-09-29 23:26:00 +0200

    - changed units to em, grouped to be able to add text

--- src/components/NodeGraph.vue
+++ src/components/NodeGraph.vue
@@ -7,6 +7,7 @@
 #my_dataviz {
   margin-top: 1em;
   justify-content: center;
+  padding-left: 4em;
 }
 
 #my_dataviz svg {
@@ -47,7 +48,7 @@ export default {
         }
       ]
 
-      let circle_radius = 20
+      let circle_radius_in_em = 1.2
       // append the svg object to the body of the page
       const svg = d3.select("#my_dataviz")
           .append("svg")
@@ -57,48 +58,59 @@ export default {
           .append("line")
           .style("stroke", "#aaa")
           .style("stroke-dasharray", ("3,5")) // make the stroke dashed
-          .attr("x1", `${circle_radius}`)
-          .attr("y1", `${circle_radius}`)
-          .attr("x2", `${circle_radius + 200}`)
-          .attr("y2", `${circle_radius}`);
+          .attr("x1", `${circle_radius_in_em}em`)
+          .attr("y1", `${circle_radius_in_em}em`)
+          .attr("x2", `${circle_radius_in_em + 12}em`)
+          .attr("y2", `${circle_radius_in_em}em`);
 
       const link2 = svg
           .append("line")
           .style("stroke", "#aaa")
           .style("stroke-dasharray", ("3,5")) // make the stroke dashed
-          .attr("x1", `${circle_radius}`)
-          .attr("y1", `${circle_radius}`)
-          .attr("x2", `${circle_radius + 200}`)
-          .attr("y2", `${circle_radius + 50}`);
+          .attr("x1", `${circle_radius_in_em}em`)
+          .attr("y1", `${circle_radius_in_em}em`)
+          .attr("x2", `${circle_radius_in_em + 12}em`)
+          .attr("y2", `${circle_radius_in_em + 4.0}em`);
 
       const supervisorGroup = svg
           .append("g")
-          .text("supervisor")
       const supervisorNode = supervisorGroup
           .append("circle")
-          .attr("r", circle_radius)
+          .attr("r", `${circle_radius_in_em}em`)
           .style("fill", "#4E616D")
-          .attr("cx", `${circle_radius}`)
-          .attr("cy", `${circle_radius}`)
-      supervisorGroup
-          .append("text")
-          .text("supervisor")
-          .attr("dy", `${2 * circle_radius + 10}`)
+          .attr("cx", `${circle_radius_in_em}em`)
+          .attr("cy", `${circle_radius_in_em}em`)
+      // supervisorGroup
+      //     .append("text")
+      //     .text("S")
+      //     .attr("dx", `${circle_radius_in_em / 2.0 + 0.1}em`)
+      //     .attr("dy", `${circle_radius_in_em + 0.1}em`)
 
-      const workerNode1 = svg
+      const worker1Group = svg.append("g")
+      const workerNode1 = worker1Group
           .append("circle")
-          .attr("r", circle_radius)
+          .attr("r", `${circle_radius_in_em}em`)
           .style("fill", "#596460")
-          .attr("cx", `${circle_radius + 200}`)
-          .attr("cy", `${circle_radius}`)
+          .attr("cx", `${circle_radius_in_em + 12}em`)
+          .attr("cy", `${circle_radius_in_em}em`)
+      // worker1Group
+      //     .append("text")
+      //     .text("W")
+      //     .attr("dx", `${circle_radius_in_em + 8.9}em`)
+      //     .attr("dy", `${circle_radius_in_em + 0.1}em`)
 
-      const workerNode2 = svg
+      const worker2Group = svg.append("g")
+      const workerNode2 = worker2Group
           .append("circle")
-          .attr("r", circle_radius)
-          // .style("fill", "#69b3a2")
+          .attr("r", `${circle_radius_in_em}em`)
           .style("fill", "#596460")
-          .attr("cx", `${circle_radius + 200}`)
-          .attr("cy", `${circle_radius + 50}`)
+          .attr("cx", `${circle_radius_in_em + 12}em`)
+          .attr("cy", `${circle_radius_in_em + 4.0}em`)
+      // worker2Group
+      //     .append("text")
+      //     .text("W")
+      //     .attr("dx", `${circle_radius_in_em + 8.9}em`)
+      //     .attr("dy", `${circle_radius_in_em + 3.3}em`)
 
     });
     return {}
