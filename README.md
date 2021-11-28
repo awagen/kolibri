6418e4167a5130acca2c3afcb085db3aabb4e134
awagen <awagen@posteo.net>
2021-11-28 20:17:44 +0100

    - adding .env file with kolibri base url and some endpoints - adjusting globalConstants.js to take env vars into account - adding some notes regarding env var handling in vite to README.md

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ Prepares resources to be served in dist folder.
 - ```docker run -p 8080:80 --rm --name kolibri-watch-1 kolibri-watch:0.1.0``` (within container, nginx listens on port 80)
 - or for non-detached: ```docker run -it -p 8080:80 --rm --name kolibri-watch-1 kolibri-watch:0.1.0```
 - access app on ```localhost:8080```
+
+## Env var management
+- vite is exposing env variables via import.meta.env (https://vitejs.dev/guide/env-and-mode.html#env-files)
+- there is priority assigned depending on which mode the app is started. Env variables that already exist when
+vue client is executed should have highest prio and not be overwritten
--- src/utils/globalConstants.js
+++ src/utils/globalConstants.js
@@ -1,6 +1,12 @@
-export const appIsUpUrl = 'http://localhost:8000/health'
-export const nodeStateUrl = 'http://localhost:8000/nodeState'
-export const jobStateUrl = 'http://localhost:8000/jobStates'
-export const jobBatchStateUrl = 'http://localhost:8000/jobAllWorkerStates'
-export const jobHistoryUrl = 'http://localhost:8000/finishedJobStates'
-export const stopJobUrl = 'http://localhost:8000/stopJob'
+// vite is exposing env variables via import.meta.env (https://vitejs.dev/guide/env-and-mode.html#env-files)
+// also there are some restrictions as to which env vars are picked up, e.g some specific values and all
+// values prefixed with VITE_.
+// In plain vue.js the prefix and the object to retrieve them by differ to the above (https://cli.vuejs.org/guide/mode-and-env.html#example-staging-mode),
+// e.g VUE_APP_ prefix and process.env object to retrieve the values from
+export const kolibriBaseUrl = import.meta.env.VITE_KOLIBRI_BASE_URL
+export const appIsUpUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_HEALTH_PATH}`
+export const nodeStateUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_NODE_STATE_PATH}`
+export const jobStateUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_JOB_STATES_PATH}`
+export const jobBatchStateUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_BATCH_STATE_PATH}`
+export const jobHistoryUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_JOB_HISTORY_PATH}`
+export const stopJobUrl = `${kolibriBaseUrl}/${import.meta.env.VITE_KOLIBRI_STOP_JOB_PATH}`
