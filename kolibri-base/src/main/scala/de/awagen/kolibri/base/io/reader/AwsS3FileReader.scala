e37a2b05ac44efda387163312b89870a0121aa8c
awagen <awagen@posteo.net>
2022-03-31 11:32:13 +0200

    - fixing aws file reader path resolution

--- kolibri-base/src/main/scala/de/awagen/kolibri/base/io/reader/AwsS3FileReader.scala
+++ kolibri-base/src/main/scala/de/awagen/kolibri/base/io/reader/AwsS3FileReader.scala
@@ -48,10 +48,10 @@ case class AwsS3FileReader(bucketName: String,
     setS3ClientIfNotSet()
     val normalizedFileIdentifier = fileIdentifier.trim match {
       case identifier if dirPathNormalized.nonEmpty && identifier.startsWith(dirPathNormalized) => identifier
-      case identifier => s"$dirPath$identifier".stripSuffix(delimiter)
+      case identifier => s"$dirPathNormalized$identifier".stripSuffix(delimiter)
     }
     val obj: S3Object = s3Client.getObject(new GetObjectRequest(bucketName, normalizedFileIdentifier))
-    val objData: S3ObjectInputStream = obj.getObjectContent;
+    val objData: S3ObjectInputStream = obj.getObjectContent
     Source.fromInputStream(objData)
   }
 
