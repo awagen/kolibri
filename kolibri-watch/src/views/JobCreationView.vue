25934f19ca17228c57219cf03b1a7bd69aac67be
awagen <awagen@posteo.net>
2022-04-11 21:28:33 +0200

    - merged data creation view and job by templates view, separated by tabs

--- kolibri-watch/src/views/JobCreationView.vue
+++ kolibri-watch/src/views/JobCreationView.vue
@@ -1,17 +1,57 @@
 <template>
-    <JobDefinitionByTemplates></JobDefinitionByTemplates>
+
+  <div class="row-container columns">
+    <ul class="tab tab-block">
+      <li>
+        <a href="#" @click="this.selectDisplay('template')">
+          <span class="choice-title">Templates</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" @click="this.selectDisplay('creation')">
+          <span class="choice-title">Parameter Composer</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <template v-if="this.isActiveTemplateView">
+    <JobDefinitionByTemplates/>
+  </template>
+  <template v-if="this.isActiveCreationView">
+    <DataCreation/>
+  </template>
+
 </template>
 
 <script>
 import JobDefinitionByTemplates from "../components/JobDefinitionByTemplates.vue";
+import DataCreation from "../components/DataCreation.vue";
 
 export default {
 
   components: {
-    JobDefinitionByTemplates
+    JobDefinitionByTemplates,
+    DataCreation
+  },
+  methods: {
+    selectDisplay(type) {
+      if (type === "template"){
+        console.info("template type")
+        this.isActiveTemplateView = true
+        this.isActiveCreationView = false
+      }
+      else if (type === "creation"){
+        console.info("creation type")
+        this.isActiveTemplateView = false
+        this.isActiveCreationView = true
+      }
+    }
   },
   data() {
     return {
+      isActiveTemplateView: true,
+      isActiveCreationView: false
     }
   },
   setup() {
@@ -21,5 +61,32 @@ export default {
 </script>
 
 <style scoped>
+
+.row-container {
+  margin: 3em;
+}
+
+.tab a .choice-title {
+  display: inline-block;
+  text-transform: uppercase;
+  color: #9C9C9C;
+  margin-right: 1.5em;
+}
+
+.tab li {
+  margin-right: 2em;
+}
+
+.tab-item.active .choice-title {
+  color: black;
+}
+
+.tab-item.active a {
+  border-bottom: none;
+}
+
+.tab a .choice-title::first-letter {
+  text-transform: uppercase;
+}
 
 </style>
