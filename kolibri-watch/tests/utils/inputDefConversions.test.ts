42a8f90b46d692453e11f2f7e93165c08e9c0b87
awagen <awagen@posteo.net>
2022-08-21 11:47:59 +0200

    - extending exports for inputDefConversions.ts, minor test addition

--- kolibri-watch/tests/utils/inputDefConversions.test.ts
+++ kolibri-watch/tests/utils/inputDefConversions.test.ts
@@ -6,7 +6,8 @@ import {expect, test} from "vitest";
 import {objToInputDef} from "../../src/utils/inputDefConversions";
 import {InputDef} from "../../src/utils/dataValidationFunctions";
 
-test("correctly convert json inputs to input defs", () => {
+
+test("correctly parse string input def", () => {
     // given
     const stringDef = {"type": "STRING"}
     // when
@@ -14,4 +15,15 @@ test("correctly convert json inputs to input defs", () => {
     // then
     expect(convertedInputDef.getInputValidation().validate("a").isValid).true
     expect(convertedInputDef.getInputValidation().validate("1").isValid).true
+})
+
+test("correctly parse regex input def", () => {
+    // given
+    const regexDef = {"type": "REGEX", "regex": "^a\\w+"}
+    // when
+    const convertedInputDef: InputDef = objToInputDef(regexDef, "el1", 1)
+    // then
+    expect(convertedInputDef.getInputValidation().validate("a").isValid).false
+    expect(convertedInputDef.getInputValidation().validate("ad").isValid).true
+    expect(convertedInputDef.getInputValidation().validate("baa").isValid).false
 })
