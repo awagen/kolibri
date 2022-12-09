c238bf96fe379ecbbe67ca8ec0d4e073a0ed28ea
awagen <awagen@posteo.net>
2022-12-09 12:11:12 +0100

    - adding visualization libs and static example page

--- kolibri-watch/src/components/DataViz.vue
+++ kolibri-watch/src/components/DataViz.vue
@@ -1,34 +1,47 @@
 <template>
 
   <div class="row-container columns">
-    <form class="form-horizontal col-6 column">
+    <!--    <form class="form-horizontal col-6 column">-->
+    <form class="form-horizontal col-12 column">
       <div class="form-group">
         <div class="col-12 col-sm-12">
           <!-- chart.js element -->
-          <div>
+          <div class="timeSeriesContainer">
             <canvas id="timelineChart"></canvas>
           </div>
         </div>
       </div>
     </form>
+    <!-- second placeholder for chart  -->
     <form class="form-horizontal col-6 column">
       <div class="form-group">
         <div class="col-12 col-sm-12">
           <!-- chart.js element -->
-          <div>
+          <div class="timeSeriesContainer">
             <canvas id="timelineChart1"></canvas>
           </div>
         </div>
       </div>
     </form>
 
-<!--    <div class="form-horizontal col-12">-->
-<!--      <form class="form-horizontal col-6 column">-->
-<!--        <h3 class="k-title">-->
-<!--          Visualization Selection-->
-<!--        </h3>-->
-<!--      </form>-->
-<!--    </div>-->
+    <form class="form-horizontal col-6 column">
+      <div class="form-group">
+        <div class="col-12 col-sm-12">
+          <!-- chart.js element -->
+          <div class="timeSeriesContainer">
+            <canvas id="barChart1"></canvas>
+          </div>
+        </div>
+      </div>
+    </form>
+
+    <!--    <div class="form-horizontal col-12">-->
+    <!--      <form class="form-horizontal col-6 column">-->
+    <!--        <h3 class="k-title">-->
+    <!--          Visualization Selection-->
+    <!--        </h3>-->
+    <!--      </form>-->
+    <!--    </div>-->
   </div>
 
 </template>
@@ -36,8 +49,15 @@
 <script>
 import Plotly from 'plotly.js-dist-min'
 import {Chart, registerables} from "chart.js";
+import {Colors} from "chart.js"
+
 Chart.register(...registerables);
+Chart.register(Colors);
 import {onMounted} from "vue";
+import dataVizTestJson from "../data/dataVizTestData.json";
+import {preservingJsonFormat} from "../utils/formatFunctions";
+import {interpolateYlGn} from "d3-scale-chromatic";
+import * as d3 from 'd3';;
 
 export default {
 
@@ -46,34 +66,68 @@ export default {
   methods: {},
   setup(props, context) {
 
-    const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-    ];
+    let numDatasets = dataVizTestJson["datasets"].length
 
     const data = {
-      labels: labels,
-      datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-      }]
+      labels: dataVizTestJson["labels"],
+
+      // for nice colors, see also:
+      // https://medium.com/code-nebula/automatically-generate-chart-colors-with-chart-js-d3s-color-scales-f62e282b2b41
+      // and
+      // https://github.com/d3/d3-scale-chromatic
+      datasets: dataVizTestJson["datasets"].map((data, index) => {
+        let color = numDatasets > 1 ? d3.interpolateCool(index / (numDatasets - 1)) : interpolateYlGn(1.0)
+        data["backgroundColor"] = color
+        data["borderColor"] = color
+        data["borderWidth"] = 1
+        data["lineTension"] = 0.2
+        data["fill"] = false
+        return data
+      })
     };
+
+    console.info(`data: ${preservingJsonFormat(data)}`)
 
     const config = {
       type: 'line',
       data: data,
-      options: {}
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          colors: {
+            enabled: false
+          }
+        }
+      }
+    };
+
+    const barConfig1 = {
+      type: 'bar',
+      data: data,
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          colors: {
+            enabled: false
+          }
+        },
+        indexAxis: 'x',
+        scales: {
+          xAxes: {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 90,
+              minRotation: 90
+            }
+          }
+        }
+      }
     };
 
     onMounted(() => {
       let chartElement = document.getElementById('timelineChart')
       let chartElement1 = document.getElementById('timelineChart1')
+      let barChartElement1 = document.getElementById('barChart1')
 
       const chart = new Chart(
           chartElement,
@@ -83,11 +137,19 @@ export default {
           chartElement1,
           config
       );
+      const barChart1 = new Chart(
+          barChartElement1,
+          barConfig1
+      );
     })
 
+    return {
+      Colors
+    }
 
 
   }
+
 
 }
 
@@ -99,6 +161,9 @@ export default {
   margin: 3em;
 }
 
+.timeSeriesContainer {
+  height: 400px;
+}
 
 
 </style>
