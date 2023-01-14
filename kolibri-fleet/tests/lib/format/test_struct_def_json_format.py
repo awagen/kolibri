32c154cec85f789a89fde3ab2d2a79efcf531be3
awagen <awagen@posteo.net>
2023-01-14 14:45:51 +0100

    - tests

--- kolibri-fleet/tests/lib/format/test_struct_def_json_format.py
+++ kolibri-fleet/tests/lib/format/test_struct_def_json_format.py
@@ -14,7 +14,8 @@
 import unittest
 
 from src.lib.format.json.struct_def_json_format import JsonKeys, StructDefTypes, StructDefJsonFormat
-from src.lib.struct.struct_def import IntStructDef, FloatStructDef, StringStructDef, BooleanStructDef
+from src.lib.struct.struct_def import IntStructDef, FloatStructDef, StringStructDef, BooleanStructDef, ListIntStructDef, \
+    ListStringStructDef
 
 
 class TestFormats:
@@ -22,6 +23,8 @@ class TestFormats:
     double_type_dict = {JsonKeys.TYPE_KEY: StructDefTypes.DOUBLE_TYPE}
     string_type_dict = {JsonKeys.TYPE_KEY: StructDefTypes.STRING_TYPE}
     bool_type_dict = {JsonKeys.TYPE_KEY: StructDefTypes.BOOLEAN_TYPE}
+    int_seq_type_dict = {JsonKeys.TYPE_KEY: StructDefTypes.INT_SEQ_TYPE}
+    string_seq_type_dict = {JsonKeys.TYPE_KEY: StructDefTypes.STRING_SEQ_TYPE}
 
 
 class TestStructDefJsonFormat(unittest.TestCase):
@@ -32,11 +35,23 @@ class TestStructDefJsonFormat(unittest.TestCase):
         double_format = StructDefJsonFormat.from_dict(TestFormats.double_type_dict)
         string_format = StructDefJsonFormat.from_dict(TestFormats.string_type_dict)
         bool_format = StructDefJsonFormat.from_dict(TestFormats.bool_type_dict)
+        int_seq_format = StructDefJsonFormat.from_dict(TestFormats.int_seq_type_dict)
+        string_seq_format = StructDefJsonFormat.from_dict(TestFormats.string_seq_type_dict)
         # then
         self.assertTrue(int_format, IntStructDef)
         self.assertTrue(double_format, FloatStructDef)
         self.assertTrue(string_format, StringStructDef)
         self.assertTrue(bool_format, BooleanStructDef)
+        self.assertTrue(int_seq_format, ListIntStructDef)
+        self.assertTrue(string_seq_format, ListStringStructDef)
+
+    def test_to_json(self):
+        # given
+        int_format = StructDefJsonFormat.from_dict(TestFormats.int_type_dict)
+        # when
+        int_format_dict = StructDefJsonFormat.to_dict(int_format)
+        # then
+        self.assertEqual(int_format_dict, TestFormats.int_type_dict)
 
 
 if __name__ == '__main__':
